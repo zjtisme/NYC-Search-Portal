331dc5bc5dab0a59031eea2ce2555b4c390681b7
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-04-02 21:43:27 -0700

    Use jest.fn() as a spy to check if the function has been called

--- nyc-search-ui/src/tests/ConfigurePage.test.js
+++ nyc-search-ui/src/tests/ConfigurePage.test.js
@@ -16,13 +16,20 @@ describe('ConfigurePage', () => {
     expect(wrapper.find('#configure-firstname')).toExist();
   });
 
-  it('should respond to change event and change the state', () => {
-    const wrapper = mount(<ConfigurePage firstName={"Tony"}/>);
+  it('should respond to change event and change the state when firstname field got changed', () => {
+    const wrapper = shallow(<ConfigurePage firstName={"Tony"}/>);
 
     wrapper.find('#configure-firstname').simulate('change', {target: {
       name: 'firstName', value: 'updated firstname'
     }});
 
     expect(wrapper.instance().state.updatedUser.firstName).toEqual('updated firstname');
+  });
+
+  test('should call handleUpdate when confirm button has been pressed', () => {
+    let handleUpdate_spy = jest.fn();
+    const wrapper = shallow(<ConfigurePage handleUpdate={handleUpdate_spy}/>);
+    wrapper.find('#configure-confirm-button').simulate('click');
+    expect(handleUpdate_spy).toHaveBeenCalled();
   });
 });
