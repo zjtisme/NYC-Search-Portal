fdb6db991f0a3d1553413197e23a539b0a60ee40
Jintai Zhang <jintaizhang@Jintais-MacBook-Pro.local>
2018-04-03 15:07:56 -0700

    Bind enter key to my search input

--- nyc-search-ui/src/components/Searchbar.js
+++ nyc-search-ui/src/components/Searchbar.js
@@ -11,10 +11,17 @@ class Searchbar extends Component {
     this.props.handleSearch(text);
   }
 
+  handleKeyPress = (e) => {
+    console.log(e.key);
+    if(e.key === 'Enter') {
+      this.doSearch();
+    }
+  }
+
   render() {
     return (
       <div className="container__header">
-        <input type="search" id="search-bar" ref="searchText" placeholder="search 50 latest news by keywords..."/>
+        <input type="search" id="search-bar" ref="searchText" placeholder="search 50 latest news by keywords..." onKeyDown={this.handleKeyPress}/>
         <button id="search-confirm-button" className="button expanded" onClick={this.doSearch}>search</button>
       </div>
     );
